ea059e01b1bf7319d6c614f96447f26556eb2705
magnuslsc <149440122+magnuslsc@users.noreply.github.com>
2024-11-09 20:54:15 +0100

    Create min_heap.c

---
+++ Playground/Stations_graph/min_heap/min_heap.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include "min_heap/min_heap.h"
+
+void heapify_down(MinHeap* heap, int index){
+    int smallest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if (left < heap->size && heap->data[left].distance < heap->data[smallest].distance){
+        smallest = left;
+    }
+
+    if (right < heap->size && heap->data[right].distance < heap->data[smallest].distance){
+        smallest = right;
+    }
+
+    if (smallest != index) {
+        swap(&heap->data[index], &heap->data[smallest]);
+        heapify_down(heap, smallest);
+    }
+}
+
+HeapNode extract_min(MinHeap *heap){
+    if (heap->size <= 0) {
+        printf("Heap is empty\n");
+        return (HeapNode){-1, -1};
+    }
+
+    HeapNode root = heap->data[0];
+    heap->data[0] = heap->data[--heap->size];
+
+    heapify_down(heap, 0);
+
+    return root;
+}
+
+void heapify_up(MinHeap* heap, int index, int* positions) {
+    while (index != 0 && heap->data[(index - 1) / 2].distance > heap->data[index].distance){
+        positions[heap->data[index].node] = (index - 1) / 2;
+        positions[heap->data[(index - 1) / 2].node] = index;
+
+        swap(&heap->data[index], &heap->data[(index - 1) / 2]);
+        index = (index - 1) / 2;
+    }
+}
+
+void insert_element(MinHeap* heap, HeapNode value, int* positions){
+    if (heap->size == MAX_SIZE) {
+        printf("Heap is full\n");
+        return;
+    }
+
+    int i = heap->size++;
+    heap->data[i] = value;
+    positions[value.node] = i;
+
+    heapify_up(heap, i, positions);
+}
+
+void decrease_key(MinHeap* heap, int node_index, int new_distance, int* positions){
+    int i = heap->positions[node_index];
+    heap->data[i].distance = new_distance;
+
+    heapify_up(heap, i, positions);
+}
