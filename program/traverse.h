6d8c5aafc1f97feaef8249cf43e16456a7467a50
magnuslsc <149440122+magnuslsc@users.noreply.github.com>
2024-11-25 15:18:06 +0100

    Delete program directory

--- program/traverse.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "data_to_graph.h"
-
-int bfs(Station** stations, int visited[], int source, int target, int* path, int num_nodes);
-int dfs(Station** stations, int source, int target, int* visited, int* path, int index);
-void print_path(Station** stations, int* path, int len);
