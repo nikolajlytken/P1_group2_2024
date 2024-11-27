1b0f6cde6d2c255e9d72481f238ad7d2ae7b4cd3
Foverskov <fover99@gmail.com>
2024-11-27 17:49:39 +0100

    deleted cache and updated gitignore

---
+++ program/build/CMakeFiles/Program.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Program.dir/data_to_graph.c.o"
+  "CMakeFiles/Program.dir/data_to_graph.c.o.d"
+  "CMakeFiles/Program.dir/main.c.o"
+  "CMakeFiles/Program.dir/main.c.o.d"
+  "CMakeFiles/Program.dir/traverse.c.o"
+  "CMakeFiles/Program.dir/traverse.c.o.d"
+  "Program"
+  "Program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
