0530574dda7878d52f6d869d7623d9796d2fbdf8
Foverskov <fover99@gmail.com>
2024-12-04 12:55:16 +0100

    viz: added dataset w. coordinates to draw

--- Viz/Visualtestdata.py
+++ Viz/Visualtestdata.py
@@ -2,15 +2,19 @@ import json
 import os
 import csv
 
+printed_messages = set()
+dir = "../DataScrape/JourneyDetail_data"
+files = os.listdir(dir)
+
 trains_dict = {}
 dataset_dict = {}
 
 def load_data(file):
-    with open(f"JourneyDetail_data/{file}",'r') as input:
+    with open(f"../DataScrape/JourneyDetail_data/{file}",'r') as input:
         data = json.load(input)
         input.close()
         return data
-    
+
 def get_line_info(data):
     trainName = data["Product"][0]["name"]
     trains_dict[trainName] = {'stops':[],'coordinates':[]}
@@ -20,19 +24,29 @@ def get_line_info(data):
         lat = station["lat"]
         trains_dict[trainName]['coordinates'].append((lon,lat))
 
+def create_dataset():
+    for train in trains_dict:
+        for i,stop in enumerate(trains_dict[train]['stops']):
+            if stop not in dataset_dict:
+                dataset_dict[stop] = trains_dict[train]["coordinates"][i]
+
 
 def write_dataset():
     with open("dataset.csv", 'w', newline='') as outfile:
         writer = csv.writer(outfile)
-        for index,station in trains_dict['IC 152']['stops']:
+        for station in dataset_dict:
             row = [station]
-            
+            row.append(dataset_dict[station][0])
+            row.append(dataset_dict[station][1])
             writer.writerow(row)
-
+    outfile.close()
 
 def main():
-    get_line_info(load_data('IC 152.json'))
-    print(trains_dict)
+    for file in files:
+        get_line_info(load_data(file))
+    create_dataset()
+
+    write_dataset()
 
 if __name__ == '__main__':
     main()
