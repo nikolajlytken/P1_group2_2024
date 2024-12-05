53f77b90b3691b492bfaeadd97d9b36d4c4d8b33
Foverskov <fover99@gmail.com>
2024-12-05 08:40:53 +0100

    Updated VIZ

--- Viz/Visualize.py
+++ Viz/Visualize.py
@@ -20,8 +20,8 @@ geo_jsons = [
     'https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_DNK_0.json',
 ]
 
-
-
+df_locations = pd.read_csv('dataset.csv')
+print(df_locations)
 geo_df_total = pd.DataFrame()
 
 for geo_json in geo_jsons:
@@ -34,7 +34,33 @@ geo_df_total.head()
 geo_df_total.plot()
 
 geo_axes = geo_df_total.plot()
-#geo_axes.scatter([20], [63], c='red')
+
+
+stations = {
+    'name':"",
+    'x': [],
+    'y': [],
+}
+
+
+for index, local in df_locations.iterrows():
+    coords = (local['lat'],local['lon'])
+    x = float(coords[1])
+    y = float(coords[0])
+
+    stations['x'].append(x)
+    stations['y'].append(y)
+
+print(len(stations))
+
+
+geo_axes = geo_df_total.plot(facecolor='#eaeaea', edgecolor='#fff', linewidth=.2, figsize=(5, 8), zorder=0)
+
+marker_size = 50
+
+#geo_axes.plot(lines['x'], lines['y'], c='teal', linewidth=.5, zorder=2)
+geo_axes.scatter(stations['x'], stations['y'], c='dimgrey', s=marker_size, marker='^', zorder=3, edgecolor='white')
 
 plt.gcf().axes[0].axis('off')
-plt.savefig('map_with_single_marker_test.jpg')
+plt.savefig('map_with_markers_and_lines.jpg')
+
