303a8192c1fb91d2c33aa3b847592ece245ca485
magnuslsc <149440122+magnuslsc@users.noreply.github.com>
2024-12-16 11:56:27 +0100

    Update shortest_path.h

--- program/shortest_path.h
+++ program/shortest_path.h
@@ -1,4 +1,5 @@
 #include "min_heap/min_heap.h"
 
-int path_find_init(Station** stations, int source, int target, int num_nodes);
-int compute_path(Station** stations, MinHeap* heap, int* path_sums, int source, int target);
+int path_find_init(Graph* network, int source, int target, int num_nodes);
+int compute_path(Station** stations, MinHeap* heap, double* path_sums, int source, int target, int** paths_id);
+void update_paths_id(Station** stations, int** paths_id, int from, int to);
