b7e2bc371f4522b8a985d193eacee4972159a53f
magnuslsc <149440122+magnuslsc@users.noreply.github.com>
2024-12-16 11:55:10 +0100

    Update data_to_graph.h

--- program/data_to_graph.h
+++ program/data_to_graph.h
@@ -3,12 +3,15 @@
 typedef struct ListNode {
     int idx_in_arr;
     int weight;
+    int edge_id;
+    int times_visited;
     struct ListNode* next;
 } ListNode;
 
 typedef struct Station {
     char name[50];
     ListNode* list_head;
+    int passengers;
 } Station;
 
 typedef struct Graph {
@@ -17,8 +20,11 @@ typedef struct Graph {
 } Graph;
 
 Graph* create_graph(char* filename, int num_stations);
-Station* create_station(char name[]);
+Station* create_station(char name[], int val);
 void create_linked_list_head(Station* station_head, int index);
 void free_adj_list(ListNode* head);
 int get_idx(Station** stations, int len, char* name);
 void print_adj_list(Graph* network);
+ListNode* find_edge(Graph* network, int edge_id);
+
+int get_edge_times_visited(int edge_id);
